3af32cd93168b0cd87f41cd6286e991cf46cb65a
LuanaLucia <luanasantos@outlook.com>
2023-04-19 15:05:30 -0300

    Continuação aula 2

---
+++ Aula-02/script.js
@@ -0,0 +1,19 @@
+let nome = "Luana";
+
+const pi = 3.1415;
+
+let pessoa = {
+    nome: 'Lucia',
+    idade: 30,
+    endereco: "av A"
+};
+
+
+console.log("oi");
+console.log(nome);
+
+nome = "Maysa";
+
+console.log(nome);
+
+console.log(pessoa.nome + ", " + pessoa.idade);
