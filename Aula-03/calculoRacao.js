c07a014253ea556780ab03acbd825d6330eb7b45
LuanaLucia <luanasantos@outlook.com>
2023-04-20 18:42:05 -0300

    aula-3

---
+++ Aula-03/calculoRacao.js
@@ -0,0 +1,11 @@
+let button = document.getElementById("calcular");
+
+button.addEventListener("mouseover", calcular);
+
+let resultado2 = document.getElementById("resultado");
+
+function calcular(){
+    console.log("oi");
+    resultado2.innerHTML = "oi";
+
+}
