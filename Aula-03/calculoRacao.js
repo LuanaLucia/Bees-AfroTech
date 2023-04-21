0bf1140b487f1c673a30785cb27221427f8b82f3
LuanaLucia <luanasantos@outlook.com>
2023-04-20 21:58:14 -0300

    aula 4

--- Aula-03/calculoRacao.js
+++ Aula-03/calculoRacao.js
@@ -8,7 +8,11 @@ let elementoResultado = document.getElementById("resultado");
 
 function calcular(){
 //interpolacao
-    elementoResultado.innerHTML = "O babbage deve comer 100g por dia"
+    let nomeCachorro = document.getElementById("nome").value;
+    //console.log(nome.nomeCachorro)
+    
+    elementoResultado.innerHTML = `${nomeCachorro} deve comer 100g por dia`
+    
     //  elementoResultado.innerHTML = "<span id='racao'>oi</span>";
 //   <span id="resultado">"innerHTML"</span>
 }
