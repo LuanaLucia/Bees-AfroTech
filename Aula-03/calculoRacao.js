95f2bd5e7a1a70949bc4e6be29d21f781e6b1236
LuanaLucia <luanasantos@outlook.com>
2023-04-22 11:27:41 -0300

    aula 5 pt1

--- Aula-03/calculoRacao.js
+++ Aula-03/calculoRacao.js
@@ -9,9 +9,12 @@ let elementoResultado = document.getElementById("resultado");
 function calcular(){
 //interpolacao
     let nomeCachorro = document.getElementById("nome").value;
+    let peso = document.getElementById("peso").value;
+
+    let qtdRacao = peso * 0.01 * 1000;
     //console.log(nome.nomeCachorro)
     
-    elementoResultado.innerHTML = `${nomeCachorro} deve comer 100g por dia`
+    elementoResultado.innerHTML = `${nomeCachorro} deve comer ${qtdRacao}g por dia`
     
     //  elementoResultado.innerHTML = "<span id='racao'>oi</span>";
 //   <span id="resultado">"innerHTML"</span>
